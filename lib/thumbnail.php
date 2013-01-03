a7ac91a98e7f68269a4833569ef55ca3b6cf68c4
RixTox <me@rixtox.com>
2013-01-03 11:27:01 +0800

    change  to

--- lib/thumbnail.php
+++ lib/thumbnail.php
@@ -34,9 +34,9 @@ class ThumbnailManager {
 		$pathinfo = pathinfo($path);
 		$thumbnail_path = "{$pathinfo['dirname']}/{$pathinfo['filename']}_{$output_size['width']}x{$output_size['height']}.{$pathinfo['extension']}";
 
-		$thumbnail_storg = \OCP\Files::getStorage('files_thumbnail');
-		if ($thumbnail_storg->file_exists($thumbnail_path)) {
-			return new \OC_Image($thumbnail_storg->getLocalFile($thumbnail_path));
+		$thumbnail_storage = \OCP\Files::getStorage('files_thumbnail');
+		if ($thumbnail_storage->file_exists($thumbnail_path)) {
+			return new \OC_Image($thumbnail_storage->getLocalFile($thumbnail_path));
 		}
 
 		$image = new \OC_Image();
@@ -52,39 +52,39 @@ class ThumbnailManager {
 			unset($image);
 			return false;
 		}
-		$l = $thumbnail_storg->getLocalFile($thumbnail_path);
+		$l = $thumbnail_storage->getLocalFile($thumbnail_path);
 
 		$image->save($l);
 		return $image;
 	}
 
 	public static function removeThumbnails($path) {
-		$thumbnail_storg = \OCP\Files::getStorage('files_thumbnail');
+		$thumbnail_storage = \OCP\Files::getStorage('files_thumbnail');
 		$pathinfo = pathinfo($path);
 		$dirname = $pathinfo['dirname'];
 
-		$handle = $thumbnail_storg->opendir($dirname);
+		$handle = $thumbnail_storage->opendir($dirname);
 
 		while($filename = readdir($handle)) { // due to the native code bug, OC_FilesystemView->readdir() can not be used.
 			if(preg_match("/^{$pathinfo['filename']}_\d*x\d*\.{$pathinfo['extension']}$/", $filename)) {
-				$thumbnail_storg->unlink($dirname.'/'.$filename);
+				$thumbnail_storage->unlink($dirname.'/'.$filename);
 			}
 
 		}
 	}
 
 	public static function renameThumbnails($oldpath, $newpath) {
-		$thumbnail_storg = \OCP\Files::getStorage('files_thumbnail');
+		$thumbnail_storage = \OCP\Files::getStorage('files_thumbnail');
 		$oldpathinfo = pathinfo($oldpath);
 		$olddirname = $oldpathinfo['dirname'];
 		$newpathinfo = pathinfo($newpath);
 		$newdirname = $newpathinfo['dirname'];
 
-		$handle = $thumbnail_storg->opendir($olddirname);
+		$handle = $thumbnail_storage->opendir($olddirname);
 
 		while($filename = readdir($handle)) { // due to the native code bug, OC_FilesystemView->readdir() can not be used.
 			if(preg_match("/^{$oldpathinfo['filename']}(_\d*x\d*\.){$oldpathinfo['extension']}$/", $filename, $matches)) {
-				$thumbnail_storg->rename("{$olddirname}/{$oldpathinfo['filename']}{$matches[1]}{$oldpathinfo['extension']}", "{$newdirname}/{$newpathinfo['filename']}{$matches[1]}{$newpathinfo['extension']}");
+				$thumbnail_storage->rename("{$olddirname}/{$oldpathinfo['filename']}{$matches[1]}{$oldpathinfo['extension']}", "{$newdirname}/{$newpathinfo['filename']}{$matches[1]}{$newpathinfo['extension']}");
 			}
 
 		}
